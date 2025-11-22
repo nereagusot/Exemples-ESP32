df2438d07ade9e8ded187e7917d41531cd7e3a8d
nerea <nereaguso@gmail.com>
2025-11-22 20:59:05 +0100

    button

---
+++ button.c
@@ -0,0 +1,44 @@
+#define BUTTON_PIN 13  // GPIO21 pin connected to button
+#define DEBOUNCE_TIME  50 // debounce time in milliseconds
+
+// Variables for debounce logic
+int lastSteadyState = LOW;
+int lastFlickerableState = LOW;
+int currentState;
+unsigned long lastDebounceTime = 0;
+
+// Comptador de pulsacions
+int counter = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+}
+
+bool button() {
+  currentState = digitalRead(BUTTON_PIN);
+
+  if (currentState != lastFlickerableState) {
+    lastDebounceTime = millis();
+    lastFlickerableState = currentState;
+  }
+
+  if ((millis() - lastDebounceTime) > DEBOUNCE_TIME) {
+    if (lastSteadyState != currentState) {
+      lastSteadyState = currentState;
+      // Retorna true si el botó està premut (LOW per INPUT_PULLUP)
+      return (currentState == LOW);
+    }
+  }
+
+  // Retorna false si no hi ha canvi o el botó no està premut
+  return false;
+}
+
+void loop() {
+  if (button()) {
+    counter++; // Incrementa el comptador
+    Serial.print("Botó premut. Comptador: ");
+    Serial.println(counter);
+  }
+}
