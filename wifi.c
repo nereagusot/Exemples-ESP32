eaf5b53142a66dc11f4293f6a9ef22355e638462
nerea <nereaguso@gmail.com>
2025-12-09 19:52:01 +0100

    wifi prueba2

--- wifi.c
+++ wifi.c
@@ -1,88 +1,54 @@
 #include <WiFi.h>
-#include <Wire.h>
-#include <LiquidCrystal_I2C.h>
-#include <PubSubClient.h> // Librería MQTT
+#include <HTTPClient.h>
+#include <ArduinoJson.h>  // Necesitas instalar esta librería desde el Library Manager
 
-LiquidCrystal_I2C LCD(0x27, 16, 2);
-
-// WiFi
 const char* ssid = "Wokwi-GUEST";
 const char* password = "";
 
-// MQTT
-const char* mqttServer = "test.mosquitto.org"; // Broker público
-const int mqttPort = 1883;
-const char* mqttTopic = "wokwi/esp32/lcd";
-
-WiFiClient espClient;
-PubSubClient client(espClient);
-
-// Spinner para mostrar conexión
-void spinner() {
-  static int8_t counter = 0;
-  const char* glyphs = "\xa1\xa5\xdb";
-  LCD.setCursor(15, 1);
-  LCD.print(glyphs[counter++]);
-  if (counter == strlen(glyphs)) counter = 0;
-}
-
-// Callback MQTT: se llama cuando llega un mensaje
-void callback(char* topic, byte* payload, unsigned int length) {
-  String msg;
-  for (int i = 0; i < length; i++) {
-    msg += (char)payload[i];
-  }
-  LCD.clear();
-  LCD.setCursor(0,0);
-  LCD.print("MQTT Message:");
-  LCD.setCursor(0,1);
-  LCD.print(msg);
-}
-
-void setupWiFi() {
-  WiFi.begin(ssid, password);
-  LCD.setCursor(0, 0);
-  LCD.print("Connecting WiFi");
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(250);
-    spinner();
-  }
-  LCD.clear();
-  LCD.setCursor(0,0);
-  LCD.print("WiFi Connected");
-  Serial.println("WiFi connected");
-}
-
-void reconnectMQTT() {
-  while (!client.connected()) {
-    Serial.print("Connecting to MQTT...");
-    if (client.connect("ESP32Client")) {
-      Serial.println("connected");
-      client.subscribe(mqttTopic); // Suscribirse al topic
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 2 seconds");
-      delay(2000);
-    }
-  }
-}
-
 void setup() {
   Serial.begin(115200);
+  WiFi.begin(ssid, password);
 
-  LCD.init();
-  LCD.backlight();
-
-  setupWiFi();
-
-  client.setServer(mqttServer, mqttPort);
-  client.setCallback(callback);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nWiFi connected!");
 }
 
 void loop() {
-  if (!client.connected()) {
-    reconnectMQTT();
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
+    http.begin("http://worldtimeapi.org/api/timezone/Europe/London");
+    int httpCode = http.GET();
+
+    if (httpCode > 0) {
+      String payload = http.getString();
+
+      // Parseamos el JSON
+      StaticJsonDocument<1024> doc;
+      DeserializationError error = deserializeJson(doc, payload);
+
+      if (!error) {
+        const char* datetime = doc["datetime"]; // Ejemplo: "2025-12-09T18:45:30.123456+00:00"
+        
+        // Separar fecha y hora
+        String date = String(datetime).substring(0, 10); // "2025-12-09"
+        String time = String(datetime).substring(11, 19); // "18:45:30"
+        
+        Serial.print("Fecha: ");
+        Serial.println(date);
+        Serial.print("Hora: ");
+        Serial.println(time);
+      } else {
+        Serial.println("Error parseando JSON");
+      }
+    } else {
+      Serial.println("Error en la petición HTTP");
+    }
+
+    http.end();
   }
-  client.loop(); // Mantiene la conexión y procesa mensajes
+
+  delay(5000); // Espera 5 segundos antes de la siguiente petición
 }
